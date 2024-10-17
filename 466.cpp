504504b3b8db0ba20147fd0aad7da4d65301725d
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2024-10-17 00:56:31 -0500

    466 - Mirror, Mirror

--- 466.cpp
+++ 466.cpp
@@ -5,62 +5,111 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void rotate90(vector<vector<char>> p) {
-    for(int i = 0; i < p.size(); i++) {
-        for(int j = 0; j < p.size(); j++) {
-            swap(p[i][j], p[j][i]);
-            swap(p[i][j], [i][p.size()-(j+1)]);
+void printMat(vector<string> &m) {
+    int n = m.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << m[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+void rotate90(vector<string> &m) {
+    int n = m.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            swap(m[i][j], m[j][i]);
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n/2; j++) {
+            swap(m[i][j], m[i][n-(j+1)]);
         }
     }
 }
-void rotate180(vector<vector<char>> p) {
-    
-}
-void rotate270(vector<vector<char>> p{
-    
-}
-void reflect(vector<vector<char>> p) {
-    
-}
-bool verifyPattern(vector<vector<char>> p, vector<vector<char>> r) {
+bool verifyPattern(vector<string> &original, vector<string> &newPattern) {
     bool ans = true;
-    for(int i = 0; i < p.size() && ans; i++) {
-        for(int j = 0; i < p.size() && ans; j++) {
-            if(p[i][j] != r[i][j]) {
+    int n = original.size();
+    for(int i = 0; i < n && ans; i++) {
+        for(int j = 0; j < n && ans; j++) {
+            if(original[i][j] != newPattern[i][j]) {
                 ans = false;
             }
         }
     }
     return ans;
 }
+void reflect(vector<string> &original) {
+    int n = original.size();
+    for(int i = 0; i < n/2; i++) {
+        for(int j = 0; j < n; j++) {
+            swap(original[i][j], original[n-(i+1)][j]);
+        }
+    }
+}
+int rotations(bool &verified, bool &reflected, vector<string> &original, vector<string> &newPattern) {
+    int deg = 0;
+    verified = verifyPattern(original, newPattern);
+    while(deg != 360 && !verified && ! reflected) {
+        rotate90(original);
+        verified = verifyPattern(original, newPattern);
+        printMat(original);
+        cout << "Verified: " << verified << endl;
+        if(!verified) {
+            reflect(original);
+            reflected = verifyPattern(original, newPattern);
+            printMat(original);
+            cout << "Reflected: " << verified << endl;
+            reflect(original);
+        }
+        deg += 90;
+    }
+    return deg;
+}
 int main() {
-    int n, k = 1;
-    bool preserved;
-    string line;
+    int n, deg, k = 1;
+    bool verified, reflected;
+    vector<string> original, newPattern;
     while(cin >> n) {
-        vector<vector<char>> p (n , vector<char> (n)), r (n , vector<char> (n));
-        preserved = true;
+        vector<string> original(n), newPattern(n);
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                cin >> p[i][j];
+                cin >> original[i][j];
             }
             for(int j = 0; j < n; j++) {
-                cin >> r[i][j];
-                if(p[i][j] != r[i][j]) {
-                    preserved = false;
-                }
+                cin >> newPattern[i][j];
             }
         }
+        verified = false, reflected = false;
+        deg = rotations(verified, reflected, original, newPattern);
+        cout << deg << endl;
         cout << "Pattern " << k << " was ";
-        if(!preserved) {
-            
-        }
-        else {
+        if(deg == 0) {
             cout << "preserved." << endl;
         }
+        else if(deg != 360) {
+            if(verified) {
+                cout << "rotated " << deg << " degrees." << endl;
+            }
+            else if(reflected) {
+                cout << "reflected vertically and rotated " << deg << " degrees." << endl;
+            }
+        }
+        else {
+            reflect(original);
+            if(verifyPattern(original, newPattern)) {
+                cout << "reflected vertically." << endl;
+            }
+            else {
+                cout << "improperly transformed." << endl;
+            }
+        }
+        original.clear();
+        newPattern.clear();
         k++;
     }
     return 0;

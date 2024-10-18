b1bcb0575d8e6e2f22570efbc5555e32fe7de79f
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2024-10-18 09:16:42 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,30 +1,93 @@
 /*
     Santiago Guevara Idarraga
-    Problem: 127 - Accordian
+    Problem: ?
 */
 
 #include <iostream>
-#include <stack>
 #include <vector>
-#include <stack>
+#include <string>
 
 using namespace std;
+using Matrix = vector<string>;
 
-int main() {
-    string c;
-    vector<stack<string>> v(52);
-    stack<string> s;
-    bool hash = false;
-    while(cin >> c && !hash) {
-        if(c == "#") {
-            hash = true;
+void rotate90(Matrix &m) {
+    int n = m.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            swap(m[i][j], m[j][i]);
         }
-        if(!hash) {
-            for(int i = 1; i < v.size(); i++) {
-                cin >> c;
-                s.push(c);
-                v[i] = c;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n/2; j++) {
+            swap(m[i][j], m[i][n-(j+1)]);
+        }
+    }
+}
+void reflect(Matrix &m) {
+    int n = m.size();
+    for(int i = 0; i < n/2; i++) {
+        for(int j = 0; j < n; j++) {
+            swap(m[i][j], m[n-(i+1)][j]);
+        }
+    }
+}
+bool isEqual(Matrix &x, Matrix &y) {
+    return x == y;
+}
+int rotate(Matrix &x, Matrix &y, bool &v, bool &r) {
+    int a;
+    bool f = false;
+    for(int i = 0; i < 3 && !f; i++) {
+        v = isEqual(x, y);
+        if(!v) {
+            reflect(x);
+            r = isEqual(x, y);
+            if(!r) {
+                reflect(x);
+                rotate90(x);
             }
+        }
+        if(v || r) {
+            f = true;
+            a = i;
+        }
+    }
+    return a;
+}
+void output(Matrix &x, Matrix &y) {
+    bool v = false, r = false;
+    int d = rotate(x, y, v, r);
+    if(deg == 0 && r) {
+         cout << "reflected vertically." << endl;
+    }
+    else {
+        if(v) {
+            cout << "rotated " << d << " degrees." << endl;
+        }
+        else if(r) {
+            cout << "reflected vertically and rotated " << d << " degrees." << endl;
+        }
+    }
+}
+int main() {
+    int n, k = 1;
+    bool p;
+    Matrix x, y;
+    while(cin >> n) {
+        b = true;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cin >> x[i][j];
+            }
+            for(int j = 0; j < n; j++) {
+                cin >> y[i][j];
+                if(x[i][j] != y[i][j]) {
+                    b = false;
+                }
+            }
+        }
+        if(!b) {
+            output(x, y);
         }
     }
     return 0;

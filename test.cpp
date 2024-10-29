89a6e719da2d30bf1230a35d3f0f72cbb9a0ad1f
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2024-10-29 09:00:41 -0500

    avances

--- test.cpp
+++ test.cpp
@@ -38,3 +38,50 @@ bool operation(Team* a, Team* b) {
     }
     return ans;
 }
+int main() {
+    int t, g, s1, s2;
+    string n, home, away;
+    char c;
+    Team *t1, *t2;
+    map<string, Team*> teams;
+    while(scanf("%d %d", &t, &g) && t != 0 && g != 0) {
+        vector<Team*> ans;
+        while(t--) {
+            cin >> n;
+            teams[n] = new Team(n);
+        }
+        while(g--) {
+            cin >> home >> s1 >> c >> s2 >> away;
+            t1 = teams[home];
+            t2 = teams[away];
+            if(s1 == s2) {
+                t1->points++;
+                t2->points++;
+            }
+            else if(s1 > s2) {
+                t1->points += 3;
+            }
+            else if(s1 < s2) {
+                t2->points += 3;
+            }
+            t1->scored += s1;
+            t2->scored += s2;
+            t1->suffered += s2;
+            t2->suffered += s1;
+            t1->games++;
+            t2->games++;
+        }
+        for(auto it = teams.begin(); it != teams.end(); it++) {
+            ans.push_back(it->second);
+        }
+        sort(ans.begin(), ans.end(), operation);
+        for(int i = 0; i < ans.size(); i++) {
+            cout << i+1
+                << ans[i]->name << ans[i]->points << ans[i]->games
+                << ans[i]->scored << ans[i]->suffered << goalDifference(ans[i])
+                << ((ans[i]->games == 0) ? "N/A" : to_string(100 * ans[i]->points/3 * ans[i]->games))
+                << endl;
+        }
+    }
+    return 0;
+}

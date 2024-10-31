6502f398fdae4676299f0660b345da09cd0ee4c0
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2024-10-31 17:50:32 -0500

    10194 - Football

--- 10194.cpp
+++ 10194.cpp
@@ -5,6 +5,8 @@
 
 #include <string>
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
@@ -16,21 +18,58 @@ struct Team {
         rank = points = games = wins = ties = losses = scored = against = 0;
     }
 };
+int difference(Team* t) {
+    return t->scored - t->against;
+}
+bool operation() {
+    bool ans;
+    return ans;
+}
 int main() {
-    int n, t, g, score1, score2;
+    int n, t, g, s1, s2;
     string tournament, team;
-    char team1[30], team2[30];
+    char team1[31], team2[31];
+    Team *t1, *t2;
+    map<string, Team*> teams;
+    vector<Team*> ans;
     while(cin >> n) {
         while(n--) {
             getline(cin, tournament);
             cin >> t;
             while(t--) {
                 getline(cin, team);
+                teams[team] = new Team(team);
             }
             cin >> g;
             while(g--) {
-                scanf("%30[^#]#%d@%d#%30[^\n]", team1, &score1, &score2, team2);
+                scanf("%30[^#]#%d@%d#%30[^\n]", team1, &s1, &s2, team2);
+                t1 = teams[team1];
+                t2 = teams[team2];
+                if(s1 == s2) {
+                    t1->points++;
+                    t2->points++;
+                    t1->ties++;
+                    t2->ties++;
+                }
+                else if(s1 > s2) {
+                    t1->points += 3;
+                    t1->wins++;
+                    t2->losses++;
+                }
+                else if(s1 < s2) {
+                    t2->points += 3;
+                    t2->wins++;
+                    t1->losses++;
+                }
+                t1->scored += s1;
+                t2->scored += s2;
+                t1->against += s2;
+                t2->against += s1;
+                t1->games++;
+                t2->games++;
             }
+            teams.clear();
+            ans.clear();
         }
     }
     return 0;

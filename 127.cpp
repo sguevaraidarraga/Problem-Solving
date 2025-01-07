5715ecefae1a2de46ce535777b4d40d73ae878d0
Santiago Guevara Idarraga <143007178+sguevaraidarraga@users.noreply.github.com>
2025-01-07 02:52:16 -0500

    127 - Accordian

--- 127.cpp
+++ 127.cpp
@@ -9,34 +9,35 @@
 
 using namespace std;
 
-bool match(string &a, string &b) {
-    return a[0] == b[0] || a[1] == b[1];
-}
-void moveStack(stack<string> &from, stack<string> &to) {
-    while(!from.empty()) {
-        to.push(from.top());
-        from.pop();
-    }
+bool match(stack<string> &a, stack<string> &b) {
+    return a.top()[0] == b.top()[0] || a.top()[1] == b.top()[1];
 }
 int main() {
     int i;
     string c;
     while(cin >> c && c != "#") {
         vector<stack<string>> v(52);
-        for(int i = 0; i < v.size(); i++) {
-            v[i].push(c);
+        v[0].push(c);
+        for(int i = 1; i < 52; i++) {
             cin >> c;
+            v[i].push(c);
         }
         i = 1;
         while(i < v.size()) {
-            if(i >= 3 && match(v[i-3].top(), v[i].top())) {
-                moveStack(v[i], v[i-3]);
-                v.erase(v.begin()+i);
+            if(i >= 3 && match(v[i-3], v[i])) {
+                v[i-3].push(v[i].top());
+                v[i].pop();
+                if(v[i].empty()) {
+                    v.erase(v.begin()+i);
+                }
                 i -= 3;
             }
-            else if(i >= 1 && match(v[i-1].top(), v[i].top())) {
-                moveStack(v[i], v[i-1]);
-                v.erase(v.begin()+i);
+            else if(i >= 1 && match(v[i-1], v[i])) {
+                v[i-1].push(v[i].top());
+                v[i].pop();
+                if(v[i].empty()) {
+                    v.erase(v.begin()+i);
+                }
                 i -= 1;
             }
             else {
@@ -47,6 +48,7 @@ int main() {
         for(int i = 0; i < v.size(); i++) {
             cout << ' ' << v[i].size();
         }
+        cout << endl;
     }
     return 0;
 }

4e106dd89cf499808215621e50d9147c9c68593c
Santiago Guevara <sguevaraidarraga@gmail.com>
2025-01-25 23:39:57 -0500

    template

---
+++ template.cpp
@@ -0,0 +1,43 @@
+/*
+    Santiago Guevara Idarraga
+    Problem: ...
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef map<int, int> mii;
+
+#define f(i, s, e) for(int i = s; i < e, i++)
+#define rf(i, e, s) for(int i = e-1; i >= s; i--)
+#define fit(it, v) for(auto it = v.begin(); it != v.end(); it++)
+#define rfit(it, v) for(auto it = v.rbegin(); it != v.rend(); it++)
+#define pb push_back
+#define mp make_pair
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define srt(v) sort(all(v))
+#define sz(v) v.size()
+#define len(s) s.lenght()
+
+#define F first
+#define S second
+
+#define debug(x) cout << #x << ": " << endl;
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main() {
+    op();
+    int cases;
+    while(cases--) {
+
+    }
+    return 0;
+}
